414f1a0feb70a600e092d59d8be1b8f47710cd6d
philipdemand <117778804+philipdemand@users.noreply.github.com>
2023-10-03 18:22:15 -0400

    update sessions and bcrypt auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
 
+    def create
+        user = User.new(user_params)
+        
+        if user.save
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: ["Unprocessable Entity"] }, status: :unprocessable_entity
+        end
+    end
+
     def index
         @users = User.all
         render json: @users
@@ -8,6 +19,12 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         render json: @user
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
     end
     
 
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
 
   resources :attendances, only: [:create, :update, :destroy]
 
+  post '/signup', to: 'users#create'
+
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
